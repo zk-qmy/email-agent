aa433f766efde25f0c61bf469ef65b49d8a308dc
My <ltquemy@gmail.com>
2026-05-01 20:24:40 +0700

    doc: update README

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.agent.graph import build_graph
+from src.agent.graph import build_graph, save_graph
 import os
 from src.agent.runner import run
 
@@ -9,5 +9,5 @@ print(__file__)
 if __name__ == "__main__":
     app = build_graph()
     # save_graph(app)
-    initial_prompt = "help me write an email to prof linh to schedule a meeting 2nd may"
+    initial_prompt = "help me write an email to prof linh to schedule a meeting on 2nd may"
     run(app, initial_prompt)
