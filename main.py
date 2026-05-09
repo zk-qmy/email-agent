3087651c1ef07a27cf5d0f85ecce52459505885e
hoangm960 <hoangm960@gmail.com>
2026-05-09 14:18:54 +0700

    fix main test file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.agent.graph import build_graph, save_graph
 import os
 from src.agent.runner import run
+import asyncio
 
 
 print(os.getcwd())
@@ -10,4 +11,4 @@ if __name__ == "__main__":
     app = build_graph()
     # save_graph(app)
     initial_prompt = "help me write an email to prof linh to schedule a meeting on 2nd may"
-    run(app, initial_prompt)
+    asyncio.run(run(app, initial_prompt))
