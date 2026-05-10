666cd163aa1c8781b3e31c859c5aefa9ef12628a
hoangm960 <hoangm960@gmail.com>
2026-05-10 13:38:00 +0700

    update main use async

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from src.agent.graph import build_graph, save_graph
+from src.agent.graph import build_graph
 import os
 from src.agent.runner import run
 import asyncio
-
-import subprocess
 
 print(os.getcwd())
 print(__file__)
@@ -16,25 +14,25 @@ if __name__ == "__main__":
     
     # Test summarize current email
     #initial_prompt = "help me summarize current email, my current user id is 1"
-    #run(app, initial_prompt)
+    # asyncio.run(run(app, initial_prompt))
     
     # Test draft + send email
     #initial_prompt = "help me write an email to prof linh to schedule a meeting on 2nd may"
-    #run(app, initial_prompt)
+    # asyncio.run(run(app, initial_prompt))
     
     # PDF valiadation
     #initial_prompt = "check this pdf if there is still something missing"
     #pdf_path = r'D:\email-agent\test-pdf\Maximum-Course-Load-Request.pdf'
-    #run(app, initial_prompt, pdf_path)
+    # asyncio.run(run(app, initial_prompt))
     
     # Test suggest department with RAG
     #initial_prompt = "I want to change my room who should i contact"
-    #run(app, initial_prompt)
+    # asyncio.run(run(app, initial_prompt))
     
     # Test ask guide with RAG
     # initial_prompt = ""
-    # run(app, initial_prompt)
+    # asyncio.run(run(app, initial_prompt))
     
     # Test many tools: should retrieve email from db, then suggest department, then draft and send
     initial_prompt = "I need to ask about the military certification draft me an email and send to the according department"
-    run(app, initial_prompt)
+    asyncio.run(run(app, initial_prompt))
